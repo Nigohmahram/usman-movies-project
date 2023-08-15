762dc7da136ae6a501b089dd570946411771b00c
Nigohmahram <usmonovmaxsudovich@gmail.com>
2023-08-15 20:21:28 +0900

    Add Hero TailwindCss plagin shadow

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -12,14 +12,17 @@ const Hero = ({trending}: HomeProps): JSX.Element => {
             setMovie(randomMovie)
       }, [trending])
   return (
-    <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:pb-12 lg:justify-end'>
+    <div className='flex flex-col space-y-2 py-23 md:space-y-4 lg:h-[65vh] lg:pb-12 lg:justify-end'>
 
       <div className='absolute top-0 left-0 h-[95vh] -z-10 w-full'>
             <Image src={`${image_base}${movie?.backdrop_path || movie?.poster_path}`} alt={movie?.title} fill/>
       </div>
 
       <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>{movie?.title || movie?.name || movie?.original_name}</h1>
-      <p className='max-w-xs md:max-w-lg lg:max-w-2xl text-xs md:text-lg lg:text-2xl'>{movie?.overview}</p>
+      <p className='max-w-xs md:max-w-lg lg:max-w-2xl text-xs text-shadow-md md:text-lg lg:text-2xl'>{movie?.overview}</p>
+      <div>
+      <button className='bg-transparent border-solid border-2 w-[200px] h-[56px] rounded-full'>Watch Now</button>
+      </div>
 
     </div>
   )
