01b4be340bc8c6061e7f315755d8ce3045dd6e82
Nigohmahram <usmonovmaxsudovich@gmail.com>
2023-08-26 05:27:43 +0900

    create Subscription

--- src/components/plan-card/plan-card.tsx
+++ src/components/plan-card/plan-card.tsx
@@ -1,8 +1,33 @@
 import { AiOutlineHourglass, AiOutlineVideoCameraAdd } from "react-icons/ai"
 import { PlanCardProps } from "./plan-card.props"
 import { RiVipCrown2Line } from "react-icons/ri"
+import { useAuth } from "@/hooks/useAuth"
+import { useContext, useState } from "react"
+import { AuthContext } from "@/context/auth.context"
 
 const PlanCard = ( { product }: PlanCardProps) => {
+	const [isLoading, setIsloading] = useState<boolean>(false)
+	const {user} = useContext(AuthContext)
+
+	const onSubmitSubscrition = async (priceId: string) => {
+		setIsloading(true)
+		const payload = {email: user?.email, priceId};
+
+		try {
+			const response = await fetch('/api/subscription', {
+				method: 'POST',
+				headers: {'Content-Type': 'application/json'},
+				body: JSON.stringify(payload),
+			});
+			const data = await response.json();
+			window.open(data.subscription.url);
+			setIsloading(false);
+
+		} catch (error) {
+			setIsloading(false)
+		}
+
+	};
   return (
                   <div  className='max-w-sm !w-[300px] !h-[450px] cursor-pointer bg-white/95 px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500'>
 						<h3 className='mb-3 text-xl font-bold text-[#6e728e]'>{product.name}</h3>
@@ -18,7 +43,13 @@ const PlanCard = ( { product }: PlanCardProps) => {
 							<div className='absolute rounded-xl left-0 right-0 bottom-0 top-0 bg-black/20 w-full h-full' />
 						</div>
 						<div className='border-[1px] border-black/20 mt-4' />
-						<button className='buyButton uppercase tracking-[1px] mt-4 w-full text-white border-[1px] border-[#6d72de] bg-[#6D72DE] py-4 rounded hover:bg-[#fff] hover:text-[#6d72de] font-bold'>Buy Plan</button>
+						<button
+onClick={() => onSubmitSubscrition(product.default_price.id)}
+className='uppercase tracking-[1px] mt-4 w-full text-white border-[1px] border-[#6d72de] bg-[#6D72DE] py-4 rounded hover:bg-[#fff] hover:text-[#6d72de] font-bold'
+disabled={isLoading}
+>
+{isLoading ? 'Loading...' : 'Buy Plan'}
+</button>
 						<div className='my-4  flex-col space-y-2'>
 							{product.metadata.adv.split(', ').map((c, id) => (
 							<div key={id} className='flex space-x-2 items-center'>
