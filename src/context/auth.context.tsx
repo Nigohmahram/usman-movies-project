b7e37e7e1aa30707b2d7fac5c60ea0349579d02f
Nigohmahram <usmonovmaxsudovich@gmail.com>
2023-08-28 05:10:30 +0900

    Fine Data

--- src/context/auth.context.tsx
+++ src/context/auth.context.tsx
@@ -5,54 +5,58 @@ import { auth } from 'src/firebase';
 import { useAuth } from 'src/hooks/useAuth';
 
 interface AuthContextState {
-        user: User | null;
-        error: string;
-        IsLoading: boolean;
-        signUp: (email: string, password: string) => Promise<void>;
-        signIn: (email: string, password: string) => Promise<void>;
-        logout: () => Promise<void>;
+	user: User | null;
+	error: string;
+	isLoading: boolean;
+	signUp: (email: string, password: string) => Promise<void>;
+	signIn: (email: string, password: string) => Promise<void>;
+	logout: () => Promise<void>;
 }
 export const AuthContext = createContext<AuthContextState>({
-        user: null,
-        error: '',
-        IsLoading: false,
-        signIn: async () => {},
-        signUp: async () => {},
-        logout:  async () => {},
+	user: null,
+	error: '',
+	isLoading: false,
+	signIn: async () => {},
+	signUp: async () => {},
+	logout: async () => {},
 });
 
-const AuthContextProvider = ({ children }: {children: ReactNode}) => {
-        const [initialLoader, setInitialLoader] = useState<boolean>(true)
-const {error, IsLoading, logout, signIn, signUp, user, setUser, setIsLoading} = useAuth();
-const router = useRouter();
+const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+	const [initialLoader, setInitialLoader] = useState<boolean>(true);
+	const { error, isLoading, logout, signIn, signUp, user, setUser, setIsLoading } = useAuth();
 
-const value = useMemo(() => ({
-        user, IsLoading, logout, signIn, error, signUp
-}),
-//eslint-disable-next-line
-[user, IsLoading, error]
-);
+	const value = useMemo(
+		() => ({
+			user,
+			isLoading,
+			logout,
+			signIn,
+			error,
+			signUp,
+		}),
 
-
-      useEffect(
-        () =>
-      onAuthStateChanged(auth, user =>  {
-        if (user) {
-                // setIsLoading(false)
-                setUser(user);
-        } else {
-                setUser(null);
-                // setIsLoading(true)
-                router.push('/auth')
-        }
-        setIsLoading(false)
-        setInitialLoader(false)
-      }),
-      //eslint-disable-next-line
-        []
-      )
+		// eslint-disable-next-line
+		[user, isLoading, error]
+	);
 
 
-return <AuthContext.Provider value={value}>{!initialLoader ? children : null}</AuthContext.Provider>
+useEffect(
+		() =>
+			onAuthStateChanged(auth, user => {
+				if (user) {
+					setUser(user);
+				} else {
+					setUser(null);
+				}
+				setIsLoading(false);
+				setInitialLoader(false);
+			}),
+
+		// eslint-disable-next-line
+		[]
+	);
+
+
+return <AuthContext.Provider value={value}>{!initialLoader ? children : null}</AuthContext.Provider>;
 }
-export default AuthContextProvider
+export default AuthContextProvider;
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,59 +2,62 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut,Use
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 import { auth } from 'src/firebase'
-import cookie from 'js-cookie';
 import Cookies from 'js-cookie';
 
 
 
-export const useAuth = (email: string, password: string) => {
-        const [IsLoading, setIsLoading] = useState<boolean>(false);
-        const [error, setError] = useState<string>('');
-        const [user, setUser] = useState<User | null>(null)
+export const useAuth = () => {
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [error, setError] = useState<string>('');
+	const [user, setUser] = useState<User | null>(null);
 
-        const router = useRouter()
+	const router = useRouter();
+	const signUp = async (email: string, password: string) => {
+		setIsLoading(true);
 
-        const signUp = async  (email: string, password: string) => {
-                setIsLoading(true)
+		await createUserWithEmailAndPassword(auth, email, password)
+			.then(res => {
+				setUser(res.user);
+				router.push('/');
+				fetch('/api/customer', {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({ email: res.user.email, user_id: res.user.uid }),
+				});
+				Cookies.set('user_id', res.user.uid);
+				setIsLoading(true);
+			})
+			.catch(error => setError(error.message))
+			.finally(() => setIsLoading(false));
+	};
 
-        await createUserWithEmailAndPassword(auth, email, password).then(res => {
-                        setUser(res.user);
-                        router.push('/');
-                        fetch ('/api/customer', {
-				method: 'POST',
-				headers: { 'Content-Type': 'application/json' },
-				body: JSON.stringify({ email: res.user.email, user_id: res.user.uid}),
-			});
-                        Cookies.set('user_id', res.user.uid)
-                        setIsLoading(true);
-                })
-                .catch(error => setError(error.message))
-                .finally(() => setIsLoading(false));
-        };
+	const signIn = async (email: string, password: string) => {
+		setIsLoading(true);
 
-        const signIn = async (email: string, password: string) => {
-                setIsLoading(true)
+		await signInWithEmailAndPassword(auth, email, password)
+			.then(res => {
+				setUser(res.user);
+				router.push('/');
+				Cookies.set('user_id', res.user.uid);
+				setIsLoading(true);
+			})
+			.catch(error => setError(error.message))
+			.finally(() => setIsLoading(false));
+	};
 
-        await signInWithEmailAndPassword(auth, email, password).then(res => {
-                        setUser(res.user);
-                        router.push('/');
-                        Cookies.set('user_id', res.user.uid)
-                        setIsLoading(true);
-                })
-                .catch(error => setError(error.message))
-                .finally(() => setIsLoading(false));
-        };
 
-        const logout = async () => {
-                setIsLoading(true)
+ 	const logout = async () => {
+		setIsLoading(true);
 
-                signOut(auth).then(() => {
-                        Cookies.remove('user_id');
-                        setUser(null);
-                        router.push('/auth');
-                })
-                .catch(error => setError(error.message))
-                .finally(() => setIsLoading(false))
-        }
-        return{error, IsLoading, user, signIn, signUp, logout, setUser, setIsLoading}
-}
+		signOut(auth)
+			.then(() => {
+				Cookies.remove('user_id');
+				setUser(null);
+				router.push('/auth');
+			})
+			.catch(error => setError(error.message))
+			.finally(() => setIsLoading(false));
+	};
+
+	return { error, isLoading, user, signIn, signUp, logout, setUser, setIsLoading };
+};
